0a51c052a5f6b08ce82b3e77b5b7181e222f63fb
tony nolan <tonynolan313@gmail.com>
2020-10-27 09:59:34 -0600

    datebase change

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var inquirer = require("inquirer")
 var mysql = require("mysql")
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "chefness",
+    database: "employeeDb"
+  });
 
-
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    questions();
+  });
 
 
 function questions(){
@@ -11,6 +22,43 @@ function questions(){
         message:"chose",
         choices:["view all employees","add employee", "view departments", "add department","add role","cancel" ],
         name:"choice"
-    }).then
+    }).then(answers =>{
+        switch(answers.choice){
+            case "view all employees":
+                employee()
+                break;
+            case "add employee" :
+                addEmployee()
+                break;
+            case "view departments":
+                departments()
+                break;
+            case "add department":
+                addDepartment()
+                break;
+            case "add role":
+                addRole()
+                break;
+            default:
+                connection.end()
+                break;                   
+        }
+    })
+}
 
+function employee(){
+
+}
+function addEmployee(){
+
+}
+function departments(){
+
+}
+function addDepartment(){
+
+}
+
+function addRole(){
+    
 }
